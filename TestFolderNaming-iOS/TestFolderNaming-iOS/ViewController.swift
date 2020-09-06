b4449efa135938d6380356a1c68dfe06578ec1b6
Niv Ben-porath <nivbp7@users.noreply.github.com>
2020-09-06 11:34:04 +0300

    initial commit

---
+++ TestFolderNaming-iOS/TestFolderNaming-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFolderNaming-iOS
+//
+//  Created by niv ben-porath on 06/09/2020.
+//  Copyright © 2020 nbp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
